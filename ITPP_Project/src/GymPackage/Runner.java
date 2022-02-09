dc2e2b54fba9e5150f0dcfab9e20ff01a68aadfc
christopher mullins <christophermullins831@gmail.com>
2022-02-09 15:45:52 +0000

    First Commit

---
+++ ITPP_Project/src/GymPackage/Runner.java
@@ -0,0 +1,15 @@
+package GymPackage;
+
+public class Runner {
+
+	//runs main method
+	public static void main(String[] args) {
+		startUp();
+	}
+
+	//runs on start up
+	private static void startUp() {
+		System.out.println("Start");
+	}
+
+}
