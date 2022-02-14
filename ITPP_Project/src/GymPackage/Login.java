fded1bdaa84070539f13781c70e8e5979aa09293
christopher mullins <christophermullins831@gmail.com>
2022-02-14 16:23:54 +0000

    made basic login screen

--- ITPP_Project/src/GymPackage/Login.java
+++ ITPP_Project/src/GymPackage/Login.java
@@ -9,10 +9,19 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField usernameEnrty;
+	private JTextField passwordEnrty;
+	private JLabel lblNewLabel;
+	
+//global variables 
+	public String username;
+	public String password;
 
 	/**
 	 * Launch the application.
@@ -45,10 +54,38 @@ public class Login extends JFrame {
 		JButton btnNewButton = new JButton("Test");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("press");
+				
+				//get values entered
+				username = usernameEnrty.getText();
+				password = passwordEnrty.getText();
+				
+				System.out.println("Username: "+username+"\nPassword: "+password);
+				
 			}
 		});
-		btnNewButton.setBounds(212, 329, 85, 21);
+		btnNewButton.setBounds(212, 329, 134, 21);
 		contentPane.add(btnNewButton);
+		
+		//username entry box
+		usernameEnrty = new JTextField();
+		usernameEnrty.setBounds(212, 106, 134, 19);
+		contentPane.add(usernameEnrty);
+		usernameEnrty.setColumns(10);
+		
+		//password entry box
+		passwordEnrty = new JTextField();
+		passwordEnrty.setBounds(212, 170, 134, 19);
+		contentPane.add(passwordEnrty);
+		passwordEnrty.setColumns(10);
+		
+		//username label
+		JLabel usernameLabel = new JLabel("Username");
+		usernameLabel.setBounds(134, 109, 68, 13);
+		contentPane.add(usernameLabel);
+		
+		//password label
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(134, 176, 68, 13);
+		contentPane.add(passwordLabel);
 	}
 }
